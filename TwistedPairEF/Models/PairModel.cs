d51922add61c31579db446c9b6788d5dbc058d0d
Tony Melvin Laptop <webn1008@gmail.com>
2022-07-17 17:33:52 -0500

    Added pair number to pairs model

--- TwistedPairEF/Models/PairModel.cs
+++ TwistedPairEF/Models/PairModel.cs
@@ -1,8 +1,13 @@
-﻿namespace TwistedPairEF.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwistedPairEF.Models
 {
     public class PairModel
     {
         public int Id { get; set; }
+        [Required]
+        [Range(26, 600)]
+        public int PairNumber { get; set; } = 0;
         public string? FirstPair { get; set; }
         public string? SecondPair { get; set; }
         public string? FirstBinderColor { get; set; }
