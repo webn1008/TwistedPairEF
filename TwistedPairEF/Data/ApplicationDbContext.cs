4900986ccce6e7c90139905fc19a5f95a5e1bfa3
Tony Melvin Laptop <webn1008@gmail.com>
2022-07-19 11:35:17 -0500

    Added seed code

--- TwistedPairEF/Data/ApplicationDbContext.cs
+++ TwistedPairEF/Data/ApplicationDbContext.cs
@@ -13,13 +13,19 @@ namespace TwistedPairEF.Data
 
         public DbSet<PairModel> PairsModel { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    ModelBuilder.Entity<PairModel>().HasData(new PairModel
-        //    {
-        //        Id = 1,
-        //        FirstPair = "White"
-        //    });
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PairModel>().HasData(new PairModel
+            {
+                //Id = 1,
+                PairNumber = 1,
+                FirstColor = "White",
+                SecondColor = "Blue",
+                FirstBinderColor = null,
+                SecondBinderColor = null
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
