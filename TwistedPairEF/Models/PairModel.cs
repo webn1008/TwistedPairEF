1f908e0c82921d6278c25b714925d6d692e2adb7
Tony Melvin <webn1008@gmail.com>
2022-07-26 08:45:30 -0400

    Removed unneeded usings

--- TwistedPairEF/Models/PairModel.cs
+++ TwistedPairEF/Models/PairModel.cs
@@ -7,7 +7,7 @@ namespace TwistedPairEF.Models
         public int Id { get; set; }
         [Required]
         [Range(26, 600)]
-        public int PairNumber { get; set; } = 0;
+        public int PairNumber { get; set; }
         public string? FirstColor { get; set; }
         public string? SecondColor { get; set; }
         public string? FirstBinderColor { get; set; }
